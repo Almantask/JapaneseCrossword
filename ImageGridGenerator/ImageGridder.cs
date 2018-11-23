890c1736cd00745bf6044000cca710ede699cd1d
Kaisinel <almantusk@gmail.com>
2018-11-23 21:49:04 +0200

    Image processing- beginning.

---
+++ ImageGridGenerator/ImageGridder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridGenerator
+{
+    public class ImageGridder
+    {
+        // TODO: to be continued.
+        public int[,] ConvertToGrid(Bitmap image)
+        {
+            var pixels = ConvertToPixelArray(image);
+            return pixels;
+        }
+
+        public int[,] ConvertToPixelArray(Bitmap image)
+        {
+            var pixels = new int[image.Height, image.Width];
+            for (var row = 0; row < image.Height; row++)
+            {
+                for (var col = 0; col < image.Width; col++)
+                {
+                    // store pixel
+                }
+            }
+
+            return pixels;
+        }
+
+        private class Pixel
+        {
+            public int Row { set; get; }
+            public int Col { set; get; }
+            public Color Color { set; get; }
+            public Pixel(int row, int col, Color color)
+            {
+                Row = row;
+                Col = col;
+                Color = color;
+            }
+        }
+    }
+}
