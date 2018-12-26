46c39cc89329b2c430ebd43cbb5cdb406a1cc980
Kaisinel <almantusk@gmail.com>
2018-12-26 20:01:55 +0200

    Messages from Singleton.

--- UnityGame/JapaneseCrossword/Assets/Scripts/Utility/MessageBoxUtil.cs
+++ UnityGame/JapaneseCrossword/Assets/Scripts/Utility/MessageBoxUtil.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,16 +17,54 @@ namespace Assets.Scripts.Utility
         void Awake()
         {
             mainPrefab = Resources.Load<GameObject>("MessageBox").GetComponent<MessageBox>();
+            mainPrefab.BindComponents();
+
+            _titles = new Dictionary<MessageType, string>()
+            {
+                {MessageType.Error, MessageType.Error.ToString()},
+                {MessageType.Warning, MessageType.Warning.ToString()},
+                {MessageType.Info, MessageType.Info.ToString()}
+            };
         }
 
         public static void Show(string message, MessageType messageType)
+        {
+            Instance.StartCoroutine(ShowE(message, messageType));
+        }
+
+        private static IEnumerator ShowE(string message, MessageType messageType)
         {
             var messageBox = Instantiate(Instance.mainPrefab.transform.gameObject);
             var messageLogic = messageBox.GetComponent<MessageBox>();
 
             messageLogic.Content = message;
             messageLogic.Title = Instance._titles[messageType];
+
+            yield return Instance.StartCoroutine(Instance.LoadMessageImage(messageType));
             messageLogic.Sprite = Instance._sprites[messageType];
+        }
+
+        private IEnumerator LoadMessageImage(MessageType type)
+        {
+            if (Instance._sprites != null && Instance._sprites.ContainsKey(type)) yield break;
+
+            var resName = GetMessageImageName(type);
+            var request = Resources.LoadAsync<Sprite>(resName);
+            while (!request.isDone)
+                yield return null;
+            var sprite = request.asset as Sprite;
+
+            if (Instance._sprites == null)
+            {
+                Instance._sprites = new Dictionary<MessageType, Sprite>();
+            }
+            Instance._sprites.Add(type, sprite);
+        }
+
+        private string GetMessageImageName(MessageType type)
+        {
+            const string prefix = "Message";
+            return $"{prefix}{type.ToString()}";
         }
     }
 }
