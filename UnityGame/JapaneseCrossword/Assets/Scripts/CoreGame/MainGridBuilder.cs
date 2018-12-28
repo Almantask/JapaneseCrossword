07cb1d0a03b5e402bc37b3fd4708774b252b1444
Kaisinel <almantusk@gmail.com>
2018-12-29 00:12:07 +0200

    Better encapsulation in main grid builder.

--- UnityGame/JapaneseCrossword/Assets/Scripts/CoreGame/MainGridBuilder.cs
+++ UnityGame/JapaneseCrossword/Assets/Scripts/CoreGame/MainGridBuilder.cs
@@ -4,35 +4,30 @@ using UnityEngine;
 
 public class MainGridBuilder:MonoBehaviour, IMainGridBuilder
 {
-    public GameBoardTile GameBoardTile;
-    public Camera Camera;
+    [SerializeField]
+    private GameBoardTile _gameBoardTile;
+    [SerializeField]
+    private float _gridWidth;
+    [SerializeField]
+    private float _gridHeight;
+    [SerializeField]
+    private float InitialTilePositionX;
+    [SerializeField]
+    private float InitialTilePositionY;
 
-    public float TileWidth;
-    public float TileHeight;
-
-    public float InitialTilePositionX;
-    public float InitialTilePositionY;
-
+    private float _tileWidth;
+    private float _tileHeight;
     private float _tileScaleX;
     private float _tileScaleY;
 
-    //private readonly float tileWidthOffset;
-    //private readonly float tileHeightOffset;
-
-    void Start()
-    {
-        _tileScaleX = TileWidth / GameBoardTile.VisualWidth;
-        _tileScaleY = TileHeight / GameBoardTile.VisualHeight;
-        GameBoardTile.transform.localScale = new Vector3(_tileScaleX, _tileScaleY);
-    }
-
     public void Build(IMonochrome[,] gridData)
     {
+
         for (var col = 0; col < gridData.GetLength(0); col++)
         {
             for (var row = 0; row < gridData.GetLength(1); row++)
             {
-                var tileObj = Instantiate(GameBoardTile, transform).gameObject;
+                var tileObj = Instantiate(_gameBoardTile, transform).gameObject;
                 tileObj.name = $"Tile [{col},{row}]";
                 RepositionTile(col, row, tileObj.transform);
 
@@ -43,10 +38,17 @@ public class MainGridBuilder:MonoBehaviour, IMainGridBuilder
         }
     }
 
+    private void AdjutTileScale()
+    {
+        _tileScaleX = _tileWidth / _gameBoardTile.VisualWidth;
+        _tileScaleY = _tileHeight / _gameBoardTile.VisualHeight;
+        _gameBoardTile.transform.localScale = new Vector3(_tileScaleX, _tileScaleY);
+    }
+
     private void RepositionTile(int col, int row, Transform tile)
     {
-        var offsetX = col * TileWidth;
-        var offsetY = row * TileHeight;
+        var offsetX = col * _tileWidth;
+        var offsetY = row * _tileHeight;
         
         var position = new Vector2(InitialTilePositionX + offsetX, InitialTilePositionY + offsetY);
         tile.position = position;
